56c2dfec8038b2777f1eabeb4b3a7fe3b952bbe3
mag389 <mag389@cornell.edu>
2020-08-03 15:29:32 +0000

    first task for file creation

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+# creates a new file with these permissions and stuff
+file { 'holberton':
+  path    => '/tmp/holberton',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
+}
---
+++ 0x0A-configuration_management/README.md
@@ -0,0 +1,1 @@
+# condfiguration management with puppet
