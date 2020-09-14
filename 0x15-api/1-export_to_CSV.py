ddf9432ad8981878680319f745e6ee2cb2d21569
mag389 <mag389@cornell.edu>
2020-09-14 20:08:12 +0000

    name guard again

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -8,21 +8,22 @@ import sys
 import urllib
 
 
-url = 'https://jsonplaceholder.typicode.com/'
-employee = sys.argv[1]
+if __name__ == "__main__":
+    url = 'https://jsonplaceholder.typicode.com/'
+    employee = sys.argv[1]
 
-r_user = requests.get(url + 'users/' + employee)
-r_task = requests.get(url + 'todos/')
+    r_user = requests.get(url + 'users/' + employee)
+    r_task = requests.get(url + 'todos/')
 
-user_dict = r_user.json()
-task_dict = r_task.json()
+    user_dict = r_user.json()
+    task_dict = r_task.json()
 
-name = user_dict['name']
-filename = employee + '.csv'
-with open(filename, 'w+', newline='') as csvfile:
-    linewriter = csv.writer(csvfile, delimiter=',', quotechar='"',
-                            quoting=csv.QUOTE_ALL)
-    for task in task_dict:
-        if task['userId'] == int(employee):
-            linewriter.writerow([employee, name,
-                                task['completed'], task['title']])
+    name = user_dict['name']
+    filename = employee + '.csv'
+    with open(filename, 'w+', newline='') as csvfile:
+        linewriter = csv.writer(csvfile, delimiter=',', quotechar='"',
+                                quoting=csv.QUOTE_ALL)
+        for task in task_dict:
+            if task['userId'] == int(employee):
+                linewriter.writerow([employee, name,
+                                    task['completed'], task['title']])
