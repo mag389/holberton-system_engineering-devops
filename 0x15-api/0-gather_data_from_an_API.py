74dad4a108d688d32e3998deaadcc4e4641c8b95
mag389 <mag389@cornell.edu>
2020-09-14 20:06:19 +0000

    main guard

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -6,34 +6,34 @@ import requests
 import sys
 import urllib
 
+if __name__ == "__main__":
+    url = 'https://jsonplaceholder.typicode.com/'
+    employee = sys.argv[1]
 
-url = 'https://jsonplaceholder.typicode.com/'
-employee = sys.argv[1]
+    r_user = requests.get(url + 'users/' + employee)
+    r_task = requests.get(url + 'todos/')
 
-r_user = requests.get(url + 'users/' + employee)
-r_task = requests.get(url + 'todos/')
+    user_dict = r_user.json()
+    task_dict = r_task.json()
+    tasks = 0
+    completed = 0
+    comp_tasks = []
+    for task in task_dict:
+        # print(type(task))
+        # print(type(task['userId']))
+        # print(type(employee))
+        if task['userId'] == int(employee):
+            tasks += 1
+            if task['completed'] is True:
+                completed += 1
+                comp_tasks.append(task)
+    # print("{}/{}".format(completed, tasks))
+    # print('-----------------')
 
-user_dict = r_user.json()
-task_dict = r_task.json()
-tasks = 0
-completed = 0
-comp_tasks = []
-for task in task_dict:
-    # print(type(task))
-    # print(type(task['userId']))
-    # print(type(employee))
-    if task['userId'] == int(employee):
-        tasks += 1
-        if task['completed'] is True:
-            completed += 1
-            comp_tasks.append(task)
-# print("{}/{}".format(completed, tasks))
-# print('-----------------')
-
-line_one = 'Employee {} is done with tasks({}/{}):'.format(
-        user_dict['name'], completed, tasks)
-print(line_one)
-for task in comp_tasks:
-    print("	 {}".format(task['title']))
-# print(r_user.text)
-# print(r_user.json)
+    line_one = 'Employee {} is done with tasks({}/{}):'.format(
+            user_dict['name'], completed, tasks)
+    print(line_one)
+    for task in comp_tasks:
+        print("	 {}".format(task['title']))
+    # print(r_user.text)
+    # print(r_user.json)
