ddc2777b7f9459207d5b605da66cfc2c3d62a162
mag389 <mag389@cornell.edu>
2020-09-14 20:11:50 +0000

    name guard for 2

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -7,25 +7,25 @@ import requests
 import sys
 import urllib
 
+if __name__ == "__main__":
+    url = 'https://jsonplaceholder.typicode.com/'
+    employee = sys.argv[1]
 
-url = 'https://jsonplaceholder.typicode.com/'
-employee = sys.argv[1]
+    r_user = requests.get(url + 'users/' + employee)
+    r_task = requests.get(url + 'todos/' + '?userId=' + employee)
 
-r_user = requests.get(url + 'users/' + employee)
-r_task = requests.get(url + 'todos/' + '?userId=' + employee)
-
-user_dict = r_user.json()
-task_dict = r_task.json()
-# print(type(task_dict))
-# print(task_dict)
-retval = {"2": []}
-task_list = []
-username = user_dict["username"]
-for task in task_dict:
-    retval['2'].append({"task": task['title'],
-                        "completed": task['completed'],
-                        "username": username})
-filename = employee + '.json'
-with open(filename, 'w', newline='') as f:
-    string = json.dumps(retval)
-    f.write(string)
+    user_dict = r_user.json()
+    task_dict = r_task.json()
+    # print(type(task_dict))
+    # print(task_dict)
+    retval = {"2": []}
+    task_list = []
+    username = user_dict["username"]
+    for task in task_dict:
+        retval['2'].append({"task": task['title'],
+                            "completed": task['completed'],
+                            "username": username})
+    filename = employee + '.json'
+    with open(filename, 'w', newline='') as f:
+        string = json.dumps(retval)
+        f.write(string)
